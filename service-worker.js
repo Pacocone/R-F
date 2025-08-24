29b8a5bfadb3a8113ab7794e7874e964230fa977
Pacocone <pacoconesa@vaquelia.com>
2025-08-24 14:07:37 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('rf-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/manifest.json'
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== 'rf-cache-v1').map(key => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
